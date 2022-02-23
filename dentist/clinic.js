99b7dc39a84fe22aacd3df10aaf26401286f1184
Arthuritease <zhaomingkoh.arthur@gmail.com>
2022-02-23 14:41:03 +0800

    did dentist hands on

---
+++ dentist/clinic.js
@@ -0,0 +1,20 @@
+[
+  {
+    clinic_id: 304,
+    address: {
+      street: "Yishun Ring road",
+      block: "456",
+      unit: "01-068",
+      Postal: "755456",
+    },
+  },
+  {
+    clinic_id: 301,
+    address: {
+      street: "Canberra Drive",
+      block: "130",
+      unit: "01-069",
+      Postal: "731130",
+    },
+  },
+];
