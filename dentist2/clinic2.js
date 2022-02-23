80cbb890571fbb678d301d52448bdaca3fc7f44a
Arthuritease <zhaomingkoh.arthur@gmail.com>
2022-02-23 14:58:35 +0800

    added all-in-one solution

---
+++ dentist2/clinic2.js
@@ -0,0 +1,33 @@
+[
+  {
+    clinic_id: 304,
+    address: {
+      street: "Yishun Ring road",
+      block: "456",
+      unit: "01-068",
+      Postal: "755456",
+    },
+    appointments: [
+      {
+        appt_id: 201,
+        patient: {
+          patient_id: 001,
+          name: "Tan Ah Kow",
+          DOB: "1994-02-02",
+          address: {
+            street: "Woodlands Drive 72",
+            block: "888",
+            unit: "08-188",
+            Postal: "730888",
+          },
+        },
+        dentist: {
+          dentst_id: 101,
+          name: "Dr Tan Ah Mao",
+        },
+        treatments:"Crowning"
+      },
+    
+    ],
+  },
+];
